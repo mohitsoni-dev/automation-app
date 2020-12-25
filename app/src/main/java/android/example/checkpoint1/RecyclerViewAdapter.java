fbefbe3ca1a4e0cecc2b019a66544d2d35bc86e0
meliodas-0 <harshsoni06@gmail.com>
2020-12-25 12:47:58 +0530

    Added RecyclerView which shows all the apps installed in the system.

---
+++ app/src/main/java/android/example/checkpoint1/RecyclerViewAdapter.java
@@ -0,0 +1,70 @@
+package android.example.checkpoint1;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<ResolveInfo> mList;
+    private LayoutInflater mLayoutInflater;
+    private PackageManager packageManager;
+
+    public RecyclerViewAdapter(Context mContext, List<ResolveInfo> mList) {
+        this.mContext = mContext;
+        this.packageManager = mContext.getPackageManager();
+        this.mLayoutInflater = LayoutInflater.from(mContext);
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = mLayoutInflater.inflate(R.layout.app_list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        ResolveInfo app = mList.get(position);
+        holder.label.setText(app.activityInfo.loadLabel(packageManager));
+        holder.icon.setImageDrawable(app.loadIcon(packageManager));
+
+        holder.itemView.setOnClickListener(v -> Toast.makeText(mContext, app.activityInfo.loadLabel(packageManager) + " is Clicked", Toast.LENGTH_SHORT).show());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView icon;
+        TextView label;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            icon = itemView.findViewById(R.id.appListItemImageView);
+            label = itemView.findViewById(R.id.appListItemTextView);
+
+        }
+    }
+}
